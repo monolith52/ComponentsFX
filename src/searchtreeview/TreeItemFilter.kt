2df2c2708663fec704ba1a54bcf2c032a01e9b26
Monolith52 <monolith52@gmail.com>
2017-03-15 08:16:49 +0900

    unbindを実装

--- src/searchtreeview/TreeItemFilter.kt
+++ src/searchtreeview/TreeItemFilter.kt
@@ -1,5 +1,6 @@
 package searchtreeview
 
+import javafx.beans.value.ChangeListener
 import javafx.beans.value.ObservableStringValue
 import javafx.scene.control.TreeItem
 import java.util.function.Predicate
@@ -14,23 +15,47 @@ import java.util.function.Predicate
  */
 class TreeItemFilter<T>(root: TreeItem<T>) {
 
+    /**
+     * ツリーアイテム間の関係を保持するリスト
+     */
     private data class Relation<T>(val parent: TreeItem<T>, val items: List<TreeItem<T>>)
     private val relations: List<Relation<T>>
+    /**
+     * 検索条件となる文字列を持つプロパティ
+     * bindメソッドによって設定してください
+     */
     private var _searchProperty: ObservableStringValue? = null
     val searchProperty: ObservableStringValue?
     get() {return _searchProperty}
+    /**
+     * 各アイテムが表示される条件式
+     */
     var predicate: Predicate<TreeItem<T>>
+    val changeListener = ChangeListener<String> { observableValue, oldV, nweV -> updateRelations() }
 
     init {
         relations = buildRelations(root)
         predicate = Predicate { it.value.toString().toLowerCase().contains(searchProperty?.value?.toLowerCase() ?: "") }
     }
 
+    /**
+     * 監視対象のテキストプロパティを設定します。
+     */
     fun bind(searchProperty: ObservableStringValue) {
         _searchProperty = searchProperty
-        searchProperty.addListener { observableValue, oldV, nweV -> updateRelations() }
+        searchProperty.addListener( changeListener )
     }
 
+    /**
+     * テキストプロパティを監視対象から外します
+     */
+    fun unbind(searchProperty: ObservableStringValue) {
+        searchProperty.removeListener( changeListener )
+    }
+
+    /**
+     * ルートを渡すと再帰的に親子関係をリストにコピーして返します
+     */
     private fun buildRelations(item: TreeItem<T>): List<Relation<T>> {
         val list = mutableListOf<Relation<T>>()
         item.children.forEach {list.addAll(buildRelations(it))}
@@ -39,13 +64,14 @@ class TreeItemFilter<T>(root: TreeItem<T>) {
         return list
     }
 
+    /**
+     * 検索条件に合った構造にツリーを変更します
+     */
     private fun updateRelations() {
-        val isWildcard = _searchProperty?.value?.trim()?.isEmpty() ?: true
-
         relations.forEach { relation ->
             var index = 0
             relation.items.forEach { item ->
-                val matched = isWildcard || predicate.test(item)
+                val matched = predicate.test(item)
                 val found = (relation.parent.children.find { it === item } != null)
 
                 if (matched || item.children.isNotEmpty()) {
